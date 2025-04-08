013ac9c471f574fbab923c3b9db36cfc681a468f
Denish099 <gyldenish@gmail.com>
2025-04-08 13:17:53 +0530

    added home page 2 components

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../lib/axios";
+import Sidebar from "../components/sidebar";
+import PostCreation from "../components/postCreation";
+
 const HomePage = () => {
-  return <div>Homepage</div>;
+  const { data: authUser } = useQuery({
+    queryKey: ["authUser"],
+  });
+  const { data: recommendedUser } = useQuery({
+    queryKey: ["recommendedUser"],
+    queryFn: async () => {
+      const response = await axiosInstance.get("/users/suggestions");
+      return response.data;
+    },
+  });
+
+  const { data: posts } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const response = await axiosInstance.get("/posts");
+      return response.data;
+    },
+  });
+
+  console.log("recommended user", recommendedUser);
+  console.log("recommended post", posts);
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+      <div className="hidden lg:block lg:colspan-1">
+        <Sidebar user={authUser} />
+      </div>
+      <div className="col-span-1 lg:col-span-2 order-first lg:order-none">
+        <PostCreation user={authUser} />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
